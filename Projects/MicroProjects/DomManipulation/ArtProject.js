9003d1863a664150af3e40be23da0d865d8f5232
Denise <denisealiciafranz@gmail.com>
2024-06-25 12:13:41 +0200

    Update

--- Projects/MicroProjects/DomManipulation/ArtProject.js
+++ Projects/MicroProjects/DomManipulation/ArtProject.js
@@ -1,18 +1,19 @@
 "use strict";
 console.log("Hello There");
 let i = 0;
-let iLimit = Math.floor(Math.random() * 20);
-let randomWords = ["Discovery", "Fullfilment", "Happiness", "Joy", "Balance"];
-let randomColor = ["red", "blue", "green", "pink", "purple", "orange", "yellow"];
+let iLimit = Math.floor(Math.random() * 20 + 5);
+let randomWords = ["Discovery", "Discovery", "Fullfilment", "Happiness", "Joy", "Balance"];
+let randomColor = ["red", "red", "blue", "green", "pink", "purple", "orange", "yellow"];
 do {
     i++;
     let newWord = document.createElement("span");
-    let wordIndex = Math.floor(Math.random() * randomWords.length - 1);
+    let wordIndex = Math.floor(Math.random() * randomWords.length);
     newWord.textContent = randomWords[wordIndex];
-    let fontSize = Math.floor(Math.random() * 500);
-    newWord.style.fontSize = String(fontSize);
-    let colorIndex = Math.floor(Math.random() * randomColor.length - 1);
+    let fontSize = String(Math.floor(Math.random() * 500 + 20));
+    newWord.style.fontSize = fontSize;
+    let colorIndex = Math.floor(Math.random() * randomColor.length);
     let color = randomColor[colorIndex];
     newWord.style.color = color;
+    console.log(color);
     document.body.appendChild(newWord);
 } while (i < iLimit);
--- Projects/MicroProjects/DomManipulation/ArtProject.ts
+++ Projects/MicroProjects/DomManipulation/ArtProject.ts
@@ -1,25 +1,26 @@
 console.log("Hello There");
 
 let i: number = 0;
-let iLimit: number = Math.floor(Math.random()*20);
+let iLimit: number = Math.floor(Math.random()*20 + 5);
 
-let randomWords = ["Discovery", "Fullfilment", "Happiness", "Joy", "Balance"];
-let randomColor = ["red", "blue", "green", "pink", "purple", "orange", "yellow"]
+let randomWords = ["Discovery", "Discovery", "Fullfilment", "Happiness", "Joy", "Balance"];
+let randomColor = ["red", "red", "blue", "green", "pink", "purple", "orange", "yellow"]
 
 
 do{
     i++
     let newWord: HTMLSpanElement = document.createElement("span");
 
-    let wordIndex: number = Math.floor(Math.random()*randomWords.length -1);
+    let wordIndex: number = Math.floor(Math.random()*randomWords.length);
     newWord.textContent = randomWords[wordIndex];
 
-    let fontSize: number = Math.floor(Math.random()*500);
-    newWord.style.fontSize = String(fontSize);
+    let fontSize: string = String(Math.floor(Math.random()*500 + 20));
+    newWord.style.fontSize = fontSize;
 
-    let colorIndex: number = Math.floor(Math.random()*randomColor.length -1);
+    let colorIndex: number = Math.floor(Math.random()*randomColor.length);
     let color: string = randomColor[colorIndex];
     newWord.style.color = color;
+    console.log(color);
 
     document.body.appendChild(newWord);
 
