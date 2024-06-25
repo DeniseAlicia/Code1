c61960d1da4e1d6c6bf77673ac7504d5ea2b1d57
Denise <denisealiciafranz@gmail.com>
2024-06-25 13:45:00 +0200

    m

--- Projects/MicroProjects/DomManipulation/ArtProject.js
+++ Projects/MicroProjects/DomManipulation/ArtProject.js
@@ -1,19 +1,30 @@
 "use strict";
 console.log("Hello There");
 let i = 0;
-let iLimit = Math.floor(Math.random() * 20 + 5);
+let iLimit = Math.floor(Math.random() * 50 + 5); //generate and round number between 20 and 0, then add 5
 let randomWords = ["Discovery", "Discovery", "Fullfilment", "Happiness", "Joy", "Balance"];
 let randomColor = ["red", "red", "blue", "green", "pink", "purple", "orange", "yellow"];
+//creating the random words
 do {
     i++;
     let newWord = document.createElement("span");
+    //pick random word from randomWords array
     let wordIndex = Math.floor(Math.random() * randomWords.length);
     newWord.textContent = randomWords[wordIndex];
-    let fontSize = String(Math.floor(Math.random() * 500 + 20));
+    //generate random font size between 0 and 150, then add 20
+    let fontSize = String(Math.floor(Math.random() * 150 + 20)) + "px"; //px as unit for css
     newWord.style.fontSize = fontSize;
+    console.log(fontSize);
+    //chooses and applies random color to the text
     let colorIndex = Math.floor(Math.random() * randomColor.length);
     let color = randomColor[colorIndex];
     newWord.style.color = color;
     console.log(color);
+    //positions the word randomly on the page
+    let positionLeft = String(Math.floor(Math.random() * 5)) + "px"; //px as unit for css
+    let positionTop = String(Math.floor(Math.random() * 500)) + "px";
+    newWord.style.position = "relative";
+    newWord.style.left = positionLeft;
+    newWord.style.top = positionTop;
     document.body.appendChild(newWord);
 } while (i < iLimit);
--- Projects/MicroProjects/DomManipulation/ArtProject.ts
+++ Projects/MicroProjects/DomManipulation/ArtProject.ts
@@ -1,27 +1,41 @@
 console.log("Hello There");
 
 let i: number = 0;
-let iLimit: number = Math.floor(Math.random()*20 + 5);
+let iLimit: number = Math.floor(Math.random()*50 + 5); //generate and round number between 20 and 0, then add 5
 
 let randomWords = ["Discovery", "Discovery", "Fullfilment", "Happiness", "Joy", "Balance"];
 let randomColor = ["red", "red", "blue", "green", "pink", "purple", "orange", "yellow"]
 
 
+//creating the random words
 do{
-    i++
+    i++;
     let newWord: HTMLSpanElement = document.createElement("span");
 
+    //pick random word from randomWords array
     let wordIndex: number = Math.floor(Math.random()*randomWords.length);
     newWord.textContent = randomWords[wordIndex];
 
-    let fontSize: string = String(Math.floor(Math.random()*500 + 20));
+    //generate random font size between 0 and 150, then add 20
+    let fontSize: string = String(Math.floor(Math.random()*150 + 20))+"px"; //px as unit for css
     newWord.style.fontSize = fontSize;
+    console.log(fontSize);
 
+    //chooses and applies random color to the text
     let colorIndex: number = Math.floor(Math.random()*randomColor.length);
     let color: string = randomColor[colorIndex];
     newWord.style.color = color;
     console.log(color);
 
+    //positions the word randomly on the page
+    let positionLeft: string = String(Math.floor(Math.random()*5)) + "px"; //px as unit for css
+    let positionTop: string = String(Math.floor(Math.random()*500)) + "px";
+    newWord.style.position = "relative";
+    newWord.style.left = positionLeft;
+    newWord.style.top =  positionTop;
+
+
     document.body.appendChild(newWord);
 
 } while (i < iLimit);
+
