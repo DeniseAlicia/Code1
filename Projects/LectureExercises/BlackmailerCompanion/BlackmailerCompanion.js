f3180bdfb3a109468880bc675035e4ffc902c4ce
Denise <denisealiciafranz@gmail.com>
2024-06-26 13:19:03 +0200

    hello

--- Projects/LectureExercises/BlackmailerCompanion/BlackmailerCompanion.js
+++ Projects/LectureExercises/BlackmailerCompanion/BlackmailerCompanion.js
@@ -1,3 +1,24 @@
 "use strict";
 console.log("Hello There");
 let chosenCharacter;
+window.addEventListener("load", handleLoad);
+function handleLoad(_event) {
+    let mail = document.querySelector("div#mail");
+    mail.addEventListener("click", placeLetter);
+    document.addEventListener("keydown", chooseCharacter);
+}
+function placeLetter(_event) {
+    //console.log(_event);
+    let x = _event.offsetX;
+    let y = _event.offsetY;
+    let mail = _event.target;
+    let letter = document.createElement("span");
+    mail.appendChild(letter);
+    letter.textContent = chosenCharacter;
+    letter.style.left = x + "px";
+    letter.style.top = y + "px";
+}
+function chooseCharacter(_event) {
+    //console.log(_event);
+    chosenCharacter = _event.key;
+}
--- Projects/LectureExercises/BlackmailerCompanion/BlackmailerCompanion.ts
+++ Projects/LectureExercises/BlackmailerCompanion/BlackmailerCompanion.ts
@@ -1,3 +1,30 @@
 console.log("Hello There");
 
 let chosenCharacter: string;
+window.addEventListener("load", handleLoad);
+
+function handleLoad(_event: Event): void{
+    let mail: HTMLElement = document.querySelector("div#mail")!;
+    mail.addEventListener("click", placeLetter);
+    document.addEventListener("keydown", chooseCharacter);
+}
+
+function placeLetter(_event: MouseEvent): void {
+    //console.log(_event);
+    let x: number = _event.offsetX;
+    let y: number = _event.offsetY;
+
+    let mail: HTMLElement = <HTMLElement> _event.target;
+    let letter: HTMLSpanElement = document.createElement("span");
+
+    mail.appendChild(letter);
+    letter.textContent = chosenCharacter;
+    letter.style.left = x + "px";
+    letter.style.top = y + "px";
+
+}
+
+function chooseCharacter(_event: KeyboardEvent): void {
+    //console.log(_event);
+    chosenCharacter = _event.key;
+}
