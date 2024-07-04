3a4b5908749a92dd3aa9d1d4997ba329d1ce81ea
Denise <denisealiciafranz@gmail.com>
2024-07-04 13:01:15 +0200

    hello

--- Projects/LectureExercises/Interaction/Confetti/confetti.js
+++ Projects/LectureExercises/Interaction/Confetti/confetti.js
@@ -3,17 +3,35 @@ console.log("Hello!");
 const canvas = document.getElementsByTagName("canvas")[0];
 const ctx = canvas.getContext("2d");
 let confetti = [];
-canvas.addEventListener("click", spawnRandomConfettiPiece);
+canvas.addEventListener("click", handleClick);
 requestAnimationFrame(animationFrame);
-function spawnRandomConfettiPiece(_event) {
+function handleClick(_event) {
+    let x = _event.offsetX;
+    let y = _event.offsetY;
+    if (_event.shiftKey == true) {
+        confettiExplosion(x, y);
+    }
+    else {
+        spawnRandomConfettiPiece(x, y);
+    }
+}
+function spawnRandomConfettiPiece(_x, _y) {
+    let direction = 1;
+    if (getRandomBoolean() == true) {
+        direction = -1;
+    }
+    let preSize = Math.floor(Math.random() * 30) + 10;
     let newConfetti = {
-        positionX: _event.offsetX,
-        positionY: _event.offsetY,
-        speedX: Math.floor(Math.random() * 5) + 1,
+        positionX: _x,
+        positionY: _y,
+        speedX: (Math.floor(Math.random() * 5) + 1) * direction,
         speedY: Math.floor(Math.random() * 5) + 1,
-        size: Math.floor(Math.random() * 30) + 10,
+        yBoost: Math.floor(Math.random() * 20) + 10,
+        size: preSize,
         color: getRandomColor(),
         circle: getRandomBoolean(),
+        bounce: 20,
+        bounceCount: 1,
     };
     confetti.push(newConfetti);
     drawConfetti(newConfetti);
@@ -34,6 +52,10 @@ function drawConfetti(_confetti) {
     }
     ctx.fill(path);
 }
+function confettiExplosion(_x, _y) {
+    for (let i = 0; i < 100; i++)
+        spawnRandomConfettiPiece(_x, _y);
+}
 function animationFrame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < confetti.length; i++) {
@@ -43,21 +65,34 @@ function animationFrame() {
             if (piece.positionY >= canvas.height - piece.size) {
                 piece.speedX = 0;
                 piece.speedY = 0;
+                if (piece.bounceCount < 12) {
+                    piece.positionY = piece.positionY - 1;
+                    piece.yBoost = piece.bounce / piece.bounceCount;
+                    piece.bounceCount += 1;
+                }
             }
             else {
                 piece.positionX = piece.positionX + piece.speedX;
-                piece.positionY = piece.positionY + piece.speedY + gravity;
+                piece.positionY = piece.positionY + piece.speedY + gravity - piece.yBoost;
             }
         }
         else {
             if (piece.positionY >= canvas.height - piece.size / 2) {
                 piece.speedX = 0;
                 piece.speedY = 0;
+                if (piece.bounceCount < 5) {
+                    piece.positionY = piece.positionY - 1;
+                    piece.yBoost = piece.bounce / piece.bounceCount;
+                    piece.bounceCount += 1;
+                }
             }
             else {
                 piece.positionX = piece.positionX + piece.speedX;
-                piece.positionY = piece.positionY + piece.speedY + gravity;
+                piece.positionY = piece.positionY + piece.speedY + gravity - piece.yBoost;
             }
+        }
+        if (piece.yBoost != 0) {
+            piece.yBoost = piece.yBoost - 1;
         }
         drawConfetti(piece);
     }
--- Projects/LectureExercises/Interaction/Confetti/confetti.ts
+++ Projects/LectureExercises/Interaction/Confetti/confetti.ts
@@ -6,33 +6,55 @@ interface ConfettiPiece {
     positionY: number,
     speedX: number,
     speedY: number,
+    yBoost: number,
     size: number,
     color: string,
     circle: boolean,
+    bounce: number,
+    bounceCount: number,
 }
 const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
 let confetti: ConfettiPiece[] = [];
 
-canvas.addEventListener("click", spawnRandomConfettiPiece);
+canvas.addEventListener("click", handleClick);
 
 requestAnimationFrame(animationFrame);
 
-function spawnRandomConfettiPiece(_event: MouseEvent): void {
+function handleClick(_event: MouseEvent): void {
 
-    
+    let x = _event.offsetX;
+    let y = _event.offsetY;
+    if( _event.shiftKey == true) {
+        confettiExplosion(x,y);
+    }
+    else {
+        spawnRandomConfettiPiece(x,y);
+    }
+}
 
+function spawnRandomConfettiPiece(_x: number, _y: number): void {
+
+    let direction: number = 1;
+    if (getRandomBoolean() == true) {
+        direction = -1;
+    }
+
+    let preSize: number = Math.floor(Math.random() * 30) + 10;
 
     let newConfetti: ConfettiPiece = {
 
-        positionX: _event.offsetX,
-        positionY: _event.offsetY,
-        speedX: Math.floor(Math.random() * 5) + 1,
+        positionX: _x,
+        positionY: _y,
+        speedX: (Math.floor(Math.random() * 5) + 1) * direction,
         speedY: Math.floor(Math.random() * 5) + 1,
-        size: Math.floor(Math.random() * 30) + 10,
+        yBoost: Math.floor(Math.random() * 20) + 10,
+        size: preSize,
         color: getRandomColor(),
         circle: getRandomBoolean(),
+        bounce: 20,
+        bounceCount: 1,
     }
 
     confetti.push(newConfetti);
@@ -64,6 +86,13 @@ function drawConfetti(_confetti: ConfettiPiece): void {
 
 }
 
+function confettiExplosion(_x: number, _y: number) {
+
+    for(let i: number = 0; i<100; i++)
+        spawnRandomConfettiPiece(_x,_y);
+
+}
+
 function animationFrame(): void {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -78,26 +107,40 @@ function animationFrame(): void {
             if (piece.positionY >= canvas.height - piece.size) {
                 piece.speedX = 0;
                 piece.speedY = 0;
+                if (piece.bounceCount < 12) { 
+                    piece.positionY = piece.positionY - 1;
+                    piece.yBoost = piece.bounce/piece.bounceCount;
+                    piece.bounceCount += 1;
+                    }
                 
             }
             else {
                 piece.positionX = piece.positionX + piece.speedX;
-                piece.positionY = piece.positionY + piece.speedY + gravity;
+                piece.positionY = piece.positionY + piece.speedY + gravity - piece.yBoost;
             }
         }
         else {
             if (piece.positionY >= canvas.height - piece.size/2) {
                 piece.speedX = 0;
                 piece.speedY = 0;
+                if (piece.bounceCount < 5) { 
+                piece.positionY = piece.positionY - 1;
+                piece.yBoost = piece.bounce/piece.bounceCount;
+                piece.bounceCount += 1;
+                }
+
+                
+                
             }
             else {
                 piece.positionX = piece.positionX + piece.speedX;  
-                piece.positionY = piece.positionY + piece.speedY + gravity;               
+                piece.positionY = piece.positionY + piece.speedY + gravity - piece.yBoost;               
             }
         }
     
-
-
+        if (piece.yBoost != 0) {
+        piece.yBoost = piece.yBoost - 1;
+        }
         drawConfetti(piece);
 
     }
